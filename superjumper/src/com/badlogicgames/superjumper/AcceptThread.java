5f9b03aa22b539c7d5f2c503c7a775ca37813250
phra <sonci3000@hotmail.com>
2013-03-21 02:49:31 +0100

    added bidirectional trasmission.

--- superjumper/src/com/badlogicgames/superjumper/AcceptThread.java
+++ superjumper/src/com/badlogicgames/superjumper/AcceptThread.java
@@ -11,37 +11,57 @@ public class AcceptThread extends Thread {
 	Socket sock;
 	int port;
 	BTsocket btsock;
+	Boolean OK2Send = false;
 	
 	public AcceptThread(int port) {
 		super();
 		this.port = port;
 	}
 	
-	
 	@Override
 	public void run () {
-		
 		try {
 			ssock = new ServerSocket(port);
 			sock =  ssock.accept();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				btsock = new BTsocket(sock.getInputStream(),sock.getOutputStream());
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
+			btsock = new BTsocket(sock.getInputStream(),sock.getOutputStream());
+			OK2Send = true;
+			new SendThread().start();
+			
+			while (true){
 				Pacco pkt = btsock.readPkt();
-				MultiplayerScreen.str = "RECV OK! " + new String(pkt.getData());
-				btsock.close();
-				try {
-					sock.close();
-				} catch (IOException e) {
-					e.printStackTrace();
+				if (pkt.getType() == 0){
+					OK2Send = false;
+					break;
 				}
+				try {
+					WorldMulti.putPaccoIn(pkt);
+				} catch (InterruptedException e) { }
+			}
+			/*
+			Pacco pkt = btsock.readPkt();
+			MultiplayerScreen.str = "RECV OK! " + new String(pkt.getData());
+			*/
+			btsock.close();
+			sock.close();
+		} catch (UnknownHostException e) {
+			MultiplayerScreen.str = "UNKNOWN HOST EXCEPTION";
+		} catch (IOException e) {
+			MultiplayerScreen.str = "IO EXCEPTION";
+		}
+	}
+	
+	private class SendThread extends Thread {
+		
+		public SendThread () {
+			super();
+		}
+
+		@Override
+		public void run () {
+			while(OK2Send){
+				try {
+					btsock.writePkt(WorldMulti.takePaccoOut());
+				} catch (InterruptedException e) { }
 			}
 		}
 	}
--- superjumper/src/com/badlogicgames/superjumper/ConnectThread.java
+++ superjumper/src/com/badlogicgames/superjumper/ConnectThread.java
@@ -10,6 +10,7 @@ public class ConnectThread extends Thread {
 	BTsocket btsock;
 	int port;
 	String dest;
+	Boolean OK2Send = false;
 	
 	public ConnectThread(String dest, int port) {
 		super();
@@ -17,27 +18,51 @@ public class ConnectThread extends Thread {
 		this.port = port;
 	}
 	
-	
 	@Override
 	public void run () {
 		String test = "TEST";
 		try {
 			sock = new Socket(dest,port);
 			btsock = new BTsocket(sock.getInputStream(),sock.getOutputStream());
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-				Pacco pkt = new Pacco(1,test.getBytes(),test.getBytes().length);
-				btsock.writePkt(pkt);
-				MultiplayerScreen.str = "SEND OK!";
-				btsock.close();
-				try {
-					sock.close();
-				} catch (IOException e) {
-					e.printStackTrace();
+			OK2Send = true;
+			new SendThread().start();
+			while (true){
+				Pacco pkt = btsock.readPkt();
+				if (pkt.getType() == 0){
+					OK2Send = false;
+					break;
 				}
+				try {
+					WorldMulti.putPaccoIn(pkt);
+				} catch (InterruptedException e) { }
+			}
+			/*
+			Pacco pkt = new Pacco(1,test.getBytes(),test.getBytes().length);
+			btsock.writePkt(pkt);
+			MultiplayerScreen.str = "SEND OK!";
+			*/
+			btsock.close();
+			sock.close();
+		} catch (UnknownHostException e) {
+			MultiplayerScreen.str = "UNKNOWN HOST EXCEPTION";
+		} catch (IOException e) {
+			MultiplayerScreen.str = "IO EXCEPTION";
+		}
+	}
+	
+	private class SendThread extends Thread {
+		
+		public SendThread () {
+			super();
+		}
+
+		@Override
+		public void run () {
+			while(OK2Send){
+				try {
+					btsock.writePkt(WorldMulti.takePaccoOut());
+				} catch (InterruptedException e) { }
+			}
 		}
 	}
 }
