f0fa1b91fa990bc7c5f45822fc46e6bb697e788b
phra <sonci3000@hotmail.com>
2013-03-22 02:51:34 +0100

    added protocol exception class.

---
+++ superjumper/src/com/badlogicgames/superjumper/ProtocolException.java
@@ -0,0 +1,14 @@
+package com.badlogicgames.superjumper;
+
+
+public class ProtocolException extends Exception {
+	
+	public ProtocolException() {
+		super();
+	}
+	
+	public ProtocolException(String msg) {
+		super(msg);
+	}
+
+}
